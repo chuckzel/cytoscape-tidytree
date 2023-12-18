9f8b6f0cfd0d5634f6b20e4b16993bbcf19f4a9f
Jan Zatloukal <xzatlouk@fi.muni.cz>
2023-12-18 10:23:22 +0100

    Add fig-viewer demo
    Also change github pages root so several demos can be shown

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,7 +17,7 @@ module.exports = {
     overrides: [
         {
             extends: ['plugin:@typescript-eslint/disable-type-checked'],
-            files: ['.eslintrc.cjs'],
+            files: ['.eslintrc.cjs', "*.js"],
         },
     ],
     "rules": {
